1aecc11e0f5465a4a800a1d985bb9776e7816aab
carlos-cocom <joaquin09cocom@gmail.com>
2025-03-23 22:37:42 -0500

    cambios minimos de diseño y colores

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router/stack";
 export default function Root() {
   return (
     <SessionProvider>
-      <Stack screenOptions={{ headerShown: false }}> {/* <-- Mueve screenOptions aquí */}
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
       </Stack>
     </SessionProvider>
